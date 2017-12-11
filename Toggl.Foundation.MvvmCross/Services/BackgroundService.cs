069c8edb9f3201e0b3da2ed3afdf7cf0e91d062c
simonrozsival <simon@rozsival.com>
2017-12-11 10:04:49 +0100

    :sparkles: Add BackgroundService

---
+++ Toggl.Foundation.MvvmCross/Services/BackgroundService.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Reactive;
+using System.Reactive.Linq;
+using System.Reactive.Subjects;
+using Toggl.Multivac;
+
+namespace Toggl.Foundation.MvvmCross.Services
+{
+    public sealed class BackgroundService : IBackgroundService
+    {
+        private readonly ITimeService timeService;
+
+        private DateTimeOffset? lastEnteredBackground { get; set; }
+        private ISubject<TimeSpan> appBecameActiveSubject { get; }
+
+        public BackgroundService(ITimeService timeService)
+        {
+            Ensure.Argument.IsNotNull(timeService, nameof(timeService));
+
+            this.timeService = timeService;
+
+            appBecameActiveSubject = new Subject<TimeSpan>();
+            lastEnteredBackground = null;
+        }
+
+        public void EnterBackground()
+            => lastEnteredBackground = timeService.CurrentDateTime;
+
+        public void EnterForeground()
+        {
+            if (lastEnteredBackground.HasValue == false)
+                return;
+
+            var timeInBackground = timeService.CurrentDateTime - lastEnteredBackground.Value;
+            lastEnteredBackground = null;
+            appBecameActiveSubject.OnNext(timeInBackground);
+        }
+
+        public IObservable<Unit> AppBecameActiveAfterAtLeast(TimeSpan timeInBackground)
+            => appBecameActiveSubject
+                .AsObservable()
+                .Where(actualTimeSpentInBackground => actualTimeSpentInBackground >= timeInBackground)
+                .Select(_ => Unit.Default);
+    }
+}
---
+++ Toggl.Foundation.MvvmCross/Services/IBackgroundService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Reactive;
+
+namespace Toggl.Foundation.MvvmCross.Services
+{
+    public interface IBackgroundService
+    {
+        void EnterBackground();
+        void EnterForeground();
+
+        IObservable<Unit> AppBecameActiveAfterAtLeast(TimeSpan timeInBackground);
+    }
+}
