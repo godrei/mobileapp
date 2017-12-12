f541d4d2beec46836cb24ce9a4e73e1bc5c13db9
simonrozsival <simon@rozsival.com>
2017-12-12 17:41:15 +0100

    :sparkles: Extract storage logic

---
+++ Toggl.PrimeRadiant/Settings/ISettingsStorage.cs
@@ -0,0 +1,13 @@
+﻿namespace Toggl.PrimeRadiant.Settings
+{
+    public interface ISettingsStorage
+    {
+        bool GetBool(string key);
+
+        string GetString(string key);
+
+        void SetBool(string key, bool value);
+
+        void SetString(string key, string value);
+    }
+}
